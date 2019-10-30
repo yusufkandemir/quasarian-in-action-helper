2e1d8b6d70e5e992274c3fd47029f01bdac5aeaf
Yusuf Kandemir <yusuf.kandemir@outlook.com.tr>
2019-10-30 10:24:51 +0300

    Added general error handling for the CLI

--- src/cli.js
+++ src/cli.js
@@ -25,15 +25,24 @@ module.exports = async () => {
   const since = subDays(new Date(), argv.days).toISOString()
   const githubFetcher = createGithubFetcher({ accessToken: process.env.ACCESS_TOKEN })
 
-  const result = await repositories.reduce(async (result, repositoryPath) => {
-    return Object.assign(await result, {
-      [repositoryPath]: {
-        issues: await parseIssues({ githubFetcher, userBlacklist, repositoryPath, since, labels }),
-        pulls: await parsePulls({ githubFetcher, userBlacklist, repositoryPath, since }),
-        releases: await parseReleases({ githubFetcher, repositoryPath, since })
-      }
-    })
-  }, Promise.resolve({}))
+  try {
+    const result = await repositories.reduce(async (result, repositoryPath) => {
+      return Object.assign(await result, {
+        [repositoryPath]: {
+          issues: await parseIssues({ githubFetcher, userBlacklist, repositoryPath, since, labels }),
+          pulls: await parsePulls({ githubFetcher, userBlacklist, repositoryPath, since }),
+          releases: await parseReleases({ githubFetcher, repositoryPath, since })
+        }
+      })
+    }, Promise.resolve({}))
+  
+    console.log(JSON.stringify(result, null, 2))
+  } catch (error) {
+    console.error(error.message)
+    if (error.response) {
+      console.error(await error.response.json())
+    }
 
-  console.log(JSON.stringify(result, null, 2))
+    process.exit(1)
+  }
 }
